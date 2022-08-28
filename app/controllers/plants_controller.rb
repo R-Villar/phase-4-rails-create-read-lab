6a9d7a108db91b7f4c4ec091dfd3ae4a0bf2c99f
Remberto Villar <rembertovillar@gmail.com>
2022-08-28 19:41:54 -0400

    commit tree 18926d10e6fe71602448e9e7bd30e6b0d7a65f55

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,24 @@
+class PlantsController < ApplicationController
+    
+    def index
+        plants = Plant.all
+        render json: plants
+    end
+
+    def create
+        plant = Plant.create(plants_params)
+        render json: plant,  status: :created
+        
+    end
+
+    def show 
+        plant = Plant.find_by(id: params[:id])
+        render json: plant
+    end
+
+    private
+
+    def plants_params
+        params.permit(:name, :image, :price)
+    end
+end
